55c034e41cb924443969d60feb9ea5050ce1bfea
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-09-13 23:00:10 +0300

    edited

--- server.js
+++ server.js
@@ -6,13 +6,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const cors=require('cors');
 app.use(cors());
-'use strict'
-
-const express=require('express');
-const app=express()
-const mongoose = require('mongoose');
-require('dotenv').config();
-const cors=require('cors');
 
 app.use(cors());
 app.use(express.json());
@@ -37,19 +30,3 @@ app.listen(PORT,()=>{
 }
 )
 
-const PORT=process.env.PORT
-const MONGO_URL=process.env.MONGO_URL;
-const DB_NAME=process.env.DB_NAME;
-
-mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
-
-const bookSeed=require('./helpers/bookSeed.helpers');
-// bookSeed();
-
-const {getbook}=require('./controllers/books.controller');
-app.get('/books',getbook)
-
-app.listen(PORT,()=>{
- console.log(`server here${PORT}`)
-}
-)
