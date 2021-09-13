defdc2c3d2180ca5b9835d5c1119cc37414e5edd
oqlaalrefai <2015978054@ses.yu.edu.jo>
2021-09-13 18:28:10 +0300

    lab_12

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -10,7 +10,35 @@ const getbook=(request,respond)=>{
     })
 }
 
+const createBook = (request, response) => {
 
-module.exports={
-    getbook
-}
+    const { title, description, email, status } = request.body;
+  
+  
+    const newbook = new bookModel({
+        title, description, email, status
+    });
+  
+    newbook.save();
+  
+    response.json(newbook);
+  
+  }
+  
+  const deleteBook = (request, response) => {
+ 
+    const bookId = request.params.Book_id;
+  
+    bookModel.deleteOne({ _id: bookId }, (error, deletedData) => {
+      if(error){alert(`you have error in backend ${error}`)}
+      else {
+      response.json(deletedData);}
+    });
+  
+  }
+  
+  module.exports = {
+    getbook,
+    createBook,
+    deleteBook
+  }
