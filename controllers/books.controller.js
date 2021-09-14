991e558499c9eb0a7530c5307f0f149988bbddf8
HaninHaidrah <haneenhadey@gmail.com>
2021-09-14 10:18:33 +0300

    using MongoAtlas

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,44 +1,44 @@
-'use strict';
+"use strict";
 
-const bookModel=require('../models/book.model')
+const bookModel = require("../models/book.model");
 
-const getbook=(request,respond)=>{
-
-    bookModel.find((error,bookdata)=>{
-        respond.json(bookdata)
-
-    })
-}
+const getbook = (request, respond) => {
+  bookModel.find((error, bookdata) => {
+    respond.json(bookdata);
+  });
+};
 
 const createBook = (request, response) => {
+  const { title, description, status, email } = request.body;
+  console.log(request.body);
 
-    const { title, description, email, status } = request.body;
-  
-  
-    const newbook = new bookModel({
-        title, description, email, status
-    });
-  
-    newbook.save();
-  
-    response.json(newbook);
-  
-  }
-  
-  const deleteBook = (request, response) => {
- 
-    const bookId = request.params.Book_id;
-  
-    bookModel.deleteOne({ _id: bookId }, (error, deletedData) => {
-      if(error){alert(`you have error in backend ${error}`)}
-      else {
-      response.json(deletedData);}
-    });
-  
-  }
-  
-  module.exports = {
-    getbook,
-    createBook,
-    deleteBook
-  }
+  const newbook = new bookModel({
+    title,
+    description,
+    status,
+    email
+  });
+
+  newbook.save();
+  console.log(newbook);
+
+  response.json(newbook);
+};
+
+const deleteBook = (request, response) => {
+  const bookId = request.params.Book_id;
+
+  bookModel.deleteOne({ _id: bookId }, (error, deletedData) => {
+    if (error) {
+      alert(`you have error in backend ${error}`);
+    } else {
+      response.json(deletedData);
+    }
+  });
+};
+
+module.exports = {
+  getbook,
+  createBook,
+  deleteBook,
+};
--- server.js
+++ server.js
@@ -1,55 +1,32 @@
-'use strict'
+"use strict";
 
-const express=require('express');
-const app=express()
-const mongoose = require('mongoose');
-require('dotenv').config();
-const cors=require('cors');
-app.use(cors());
-'use strict'
-
-const express=require('express');
-const app=express()
-const mongoose = require('mongoose');
-require('dotenv').config();
-const cors=require('cors');
-
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+require("dotenv").config();
+const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const PORT=process.env.PORT
-const MONGO_URL=process.env.MONGO_URL;
-const DB_NAME=process.env.DB_NAME;
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+const DB_NAME = process.env.DB_NAME;
 
-mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
+mongoose.connect(`${MONGO_URL}`);
 
-const bookSeed = require('./helpers/bookSeed.helpers');
+const bookSeed = require("./helpers/bookSeed.helpers");
 // bookSeed();
 
-const {getbook, createBook, deleteBook}=require('./controllers/books.controller');
-app.get('/books',getbook)
+const {
+  getbook,
+  createBook,
+  deleteBook,
+} = require("./controllers/books.controller");
+app.get("/books", getbook);
 
-app.post('/books', createBook);
-app.delete('/books/:Book_id', deleteBook);
+app.post("/books", createBook);
+app.delete("/books/:Book_id", deleteBook);
 
-app.listen(PORT,()=>{
- console.log(`server here${PORT}`)
-}
-)
-
-const PORT=process.env.PORT
-const MONGO_URL=process.env.MONGO_URL;
-const DB_NAME=process.env.DB_NAME;
-
-mongoose.connect(`${MONGO_URL}/${DB_NAME}`);
-
-const bookSeed=require('./helpers/bookSeed.helpers');
-// bookSeed();
-
-const {getbook}=require('./controllers/books.controller');
-app.get('/books',getbook)
-
-app.listen(PORT,()=>{
- console.log(`server here${PORT}`)
-}
-)
+app.listen(PORT, () => {
+  console.log(`server here${PORT}`);
+});
